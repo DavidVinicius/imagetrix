872f39fb11cae78ffe9cc836dfac1377daae51fc
David Vinicius <de.175@hotmail.com>
2020-03-02 08:42:23 -0300

    Configuration files.

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+
+require __DIR__."/vendor/autoload.php";
+
+use Imagetrix\Imagetrix;
+use NumPHP\Core\NumArray;
+
+
+//$matrix  = (new NumArray(Imagetrix::toMatrixBinary("./image2.png")));
+//$matrixA = (Imagetrix::toMatrixRGB("./image2.png"));
+
+//var_dump($matrixA);
+
+print_r(Imagetrix::toBinaryMatrix("./image2.png", 128));
+
+//echo Imagetrix::draw($matrixA);
+
+//print_r(Imagetrix::pixelAt("./image.png", 100, 100));
